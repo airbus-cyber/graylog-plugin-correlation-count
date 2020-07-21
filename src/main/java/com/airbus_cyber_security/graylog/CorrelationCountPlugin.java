daf4d1552d308cbefdcb332e39434d60e53231f4
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-21 11:28:04 +0200

    Moved Module into events namespace

--- src/main/java/com/airbus_cyber_security/graylog/CorrelationCountPlugin.java
+++ src/main/java/com/airbus_cyber_security/graylog/CorrelationCountPlugin.java
@@ -1,12 +1,12 @@
 package com.airbus_cyber_security.graylog;
 
+import com.airbus_cyber_security.graylog.events.CorrelationCountModule;
 import org.graylog2.plugin.Plugin;
 import org.graylog2.plugin.PluginMetaData;
 import org.graylog2.plugin.PluginModule;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 public class CorrelationCountPlugin implements Plugin {
     @Override
