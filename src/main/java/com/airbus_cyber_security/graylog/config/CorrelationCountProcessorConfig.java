ccca9a9cf17cf558f1b5b32c7b534ce455bf1767
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-21 11:32:03 +0200

    Moved Module into contentpack.entity namespace

--- src/main/java/com/airbus_cyber_security/graylog/config/CorrelationCountProcessorConfig.java
+++ src/main/java/com/airbus_cyber_security/graylog/config/CorrelationCountProcessorConfig.java
@@ -1,5 +1,6 @@
 package com.airbus_cyber_security.graylog.config;
 
+import com.airbus_cyber_security.graylog.events.contentpack.entities.CorrelationCountProcessorConfigEntity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
