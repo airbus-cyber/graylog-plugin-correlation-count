cbda5acca00fc345a646e33df62844ef2fa6c54c
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-22 15:33:29 +0200

    Use webdriver screenshot facility in end-to-end tests

--- validation/step_impl/step_impl.py
+++ validation/step_impl/step_impl.py
@@ -1,4 +1,5 @@
 from getgauge.python import before_suite, after_suite, step
+from getgauge.python import custom_screenshot_writer
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -48,3 +49,10 @@ def fill_input(identifier, value):
     WebDriverWait(driver, 10).until(presence_of_element_located([By.ID, identifier]))
     driver.find_element_by_id(identifier).send_keys(value)
 
+@custom_screenshot_writer
+def take_screenshot():
+    image = driver.get_screenshot_as_png()
+    file_name = os.path.join(os.getenv("gauge_screenshots_dir"), "screenshot-{0}.png".format(uuid1().int))
+    file = open(file_name, "wb")
+    file.write(image)
+    return os.path.basename(file_name)
