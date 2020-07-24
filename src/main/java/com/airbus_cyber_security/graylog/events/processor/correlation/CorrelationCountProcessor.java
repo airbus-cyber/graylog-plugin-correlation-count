adc39ffc113ae553693f6fd14d399899b68c85d2
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-24 15:38:13 +0200

    Removed commented out code

--- src/main/java/com/airbus_cyber_security/graylog/events/processor/correlation/CorrelationCountProcessor.java
+++ src/main/java/com/airbus_cyber_security/graylog/events/processor/correlation/CorrelationCountProcessor.java
@@ -83,15 +83,6 @@ public class CorrelationCountProcessor implements EventProcessor {
 
     @Override
     public void sourceMessagesForEvent(Event event, Consumer<List<MessageSummary>> messageConsumer, long limit) throws EventProcessorException {
-        /*CorrelationCountCheckResult correlationCountCheckResult = getCorrelationCountCheckResult(searches, config);
-
-        List<MessageSummary> messageSummaries = correlationCountCheckResult.getMessageSummaries();
-        if (correlationCountCheckResult != null) {
-            if(correlationCountCheckResult.getMessageSummaries().size() > limit) {
-                messageSummaries = correlationCountCheckResult.getMessageSummaries().subList(0, Math.toIntExact(limit));
-            }
-            consumer.accept(messageSummaries);
-        }*/
         if (config.messageBacklog()>0) {//TODO condition should be the one triggered
             if (limit <= 0) {
                 return;
