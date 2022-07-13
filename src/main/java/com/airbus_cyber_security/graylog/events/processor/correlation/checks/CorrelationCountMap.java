9694237d1978443abc216635097ab295d495667e
c8y3 <25362953+c8y3@users.noreply.github.com>
2022-07-13 11:22:26 +0200

    Removed done TODO

--- src/main/java/com/airbus_cyber_security/graylog/events/processor/correlation/checks/CorrelationCountMap.java
+++ src/main/java/com/airbus_cyber_security/graylog/events/processor/correlation/checks/CorrelationCountMap.java
@@ -22,7 +22,6 @@ import java.util.Map;
 
 public class CorrelationCountMap {
 
-    // TODO change Long[] into a new type CorrelationResult which will contain the timestamp, group by fields, first stream count and second stream count
     private final Map<String, CorrelationCountResult> results;
 
     public CorrelationCountMap() {
@@ -34,13 +33,13 @@ public class CorrelationCountMap {
         this.results.put(groupByFields, result);
     }
 
-    // TODO add timestamp, change time of groupByFields to ImmutableList<String>
+    // TODO add timestamp, change type of groupByFields to ImmutableList<String>
     public void addFirstStreamCount(String groupByFields, long firstStreamCount) {
         long secondStreamCount = 0L;
         this.setResult(groupByFields, firstStreamCount, secondStreamCount);
     }
 
-    // TODO add timestamp, change time of groupByFields to ImmutableList<String>
+    // TODO add timestamp, change type of groupByFields to ImmutableList<String>
     public void addSecondStreamCount(String groupByFields, long secondStreamCount) {
         long firstStreamCount = 0L;
         CorrelationCountResult previousResult = this.results.get(groupByFields);
